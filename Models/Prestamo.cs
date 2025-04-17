535fac15b63a9601115baa118ee2df7095ffac25
Ale Blanco <kookieeeismine@gmail.com>
2025-04-17 17:39:49 -0400

    Agregar validaciones de Data Annotations/Entity Framework

--- Models/Prestamo.cs
+++ Models/Prestamo.cs
@@ -4,10 +4,17 @@ namespace Biblioteca2.Models
 {
     public class Prestamo
     {
+        [Required(ErrorMessage = "El ISBN es obligatorio")]
+        [StringLength(17, MinimumLength = 10)]
         public string ISBN { get; set; }
-        public string Usuario { get; set; }
-        public DateTime Fecha { get; set; }
 
+        [Required]
+        [MaxLength(100)]
+        public string Usuario { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; } = DateTime.Now; // Valor por defecto
+        
         public Prestamo() { }
 
         public Prestamo(string isbn, string usuario, DateTime fecha)
